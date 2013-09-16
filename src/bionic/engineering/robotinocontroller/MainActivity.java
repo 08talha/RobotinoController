0d70fe5cfc9fd67b28dce2da3b845564a0e825bb
andersndanielsen <andersndanielsen@gmail.com>
2013-09-16 15:33:51 +0200

    Added Socket

--- src/bionic/engineering/robotinocontroller/MainActivity.java
+++ src/bionic/engineering/robotinocontroller/MainActivity.java
@@ -1,5 +1,13 @@
 package bionic.engineering.robotinocontroller;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -12,19 +20,26 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity{
-	
+
 	private GyroVisualizer mGyroView;
+
+	private Socket socket;
+	private static final int SERVERPORT = 5000;
+	private static final String SERVER_IP = "10.0.2.2";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		hookupSensorListener();
-		
+
 		mGyroView = new GyroVisualizer(this);
-		//setContentView(mGyroView);
 		LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
 		layout.addView(mGyroView);
+		new Thread(new ClientThread()).start();
+		
+		connect();
+
 	}
 
 	@Override
@@ -33,65 +48,94 @@ public class MainActivity extends Activity{
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	private void hookupSensorListener() 
+
+	private void hookupSensorListener()
 	{
-		SensorManager sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		sm.registerListener(mGyroListener, sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_UI);
 	}
 
-	private SensorEventListener mGyroListener = new SensorEventListener() {
+	private SensorEventListener mGyroListener = new SensorEventListener(){
 
-        private static final float MIN_TIME_STEP = (1f / 40f);
-        private long mLastTime = System.currentTimeMillis();
-        private float mRotationX, mRotationY, mRotationZ;
+		private static final float MIN_TIME_STEP = (1f / 40f);
+		private long mLastTime = System.currentTimeMillis();
+		private float mRotationX, mRotationY, mRotationZ;
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy){
+		}
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-            int i = 1;
+		@Override
+		public void onSensorChanged(SensorEvent event){
+			float[] values = event.values;
+			float x = values[0];
+			float y = values[1];
+			float z = values[2];
 
-            float angularVelocity = z * 0.96f; // Minor adjustment to avoid drift on Nexus S
+			float angularVelocity = z * 0.96f; // Minor adjustment to avoid drift on Nexus S
 
-            // Calculate time diff
-            long now = System.currentTimeMillis();
-            float timeDiff = (now - mLastTime) / 1000f;
-            mLastTime = now;
-            if (timeDiff > 1) {
-                // Make sure we don't go bananas after pause/resume
-                timeDiff = MIN_TIME_STEP;
-            } 
+			// Calculate time diff
+			long now = System.currentTimeMillis();
+			float timeDiff = (now - mLastTime) / 1000f;
+			mLastTime = now;
+			if(timeDiff > 1){
+				// Make sure we don't go bananas after pause/resume
+				timeDiff = MIN_TIME_STEP;
+			}
 
-            mRotationX += x * timeDiff;
-            if (mRotationX > 0.5f)
-                mRotationX = 0.5f;
-            else if (mRotationX < -0.5f)
-                mRotationX = -0.5f;
+			mRotationX += x * timeDiff;
+			if(mRotationX > 0.5f)
+				mRotationX = 0.5f;
+			else if(mRotationX < -0.5f)
+				mRotationX = -0.5f;
 
-            mRotationY += y * timeDiff;
-            if (mRotationY > 0.5f)
-                mRotationY = 0.5f;
-            else if (mRotationY < -0.5f)
-                mRotationY = -0.5f;
+			mRotationY += y * timeDiff;
+			if(mRotationY > 0.5f)
+				mRotationY = 0.5f;
+			else if(mRotationY < -0.5f)
+				mRotationY = -0.5f;
 
-            mRotationZ += angularVelocity * timeDiff;
+			mRotationZ += angularVelocity * timeDiff;
 
-            mGyroView.setGyroRotation(mRotationX, mRotationY, mRotationZ);
-            updateOrientation(mRotationX, mRotationY, mRotationZ);
-        }
-    };
+			mGyroView.setGyroRotation(mRotationX, mRotationY, mRotationZ);
+			updateOrientation(mRotationX, mRotationY, mRotationZ);
+		}
+	};
 
-	private void updateOrientation(float x, float y, float z) 
+	private void updateOrientation(float x, float y, float z)
 	{
-		  TextView output = (TextView)findViewById(R.id.output);
-		  output.setText("x: " + x + "\ny: " + y + "\nz: " + z);
-		  
+		TextView output = (TextView) findViewById(R.id.output);
+		output.setText("x: " + x + "\ny: " + y + "\nz: " + z);
+
 	}
-}//Andy
+
+	public void connect(){
+		try{
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream())),
+					true);
+			out.println("HELLO MAG!");
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	class ClientThread implements Runnable{
+		@Override
+		public void run(){
+			try{
+				InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+				socket = new Socket(serverAddr, SERVERPORT);
+
+			}catch(UnknownHostException e1){
+				e1.printStackTrace();
+			}catch(IOException e1){
+				e1.printStackTrace();
+			}
+		}
+	}
+}
