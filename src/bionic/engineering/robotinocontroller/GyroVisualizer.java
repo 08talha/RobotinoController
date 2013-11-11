ae10021ea135a9c68eaea5941140b3c38fa587f5
andersndanielsen <andersndanielsen@gmail.com>
2013-11-11 14:49:04 +0100

    size of circle

--- src/bionic/engineering/robotinocontroller/GyroVisualizer.java
+++ src/bionic/engineering/robotinocontroller/GyroVisualizer.java
@@ -9,16 +9,27 @@ import android.view.View;
 
 public class GyroVisualizer extends View {
     private static final float RADIANS_TO_DEGREES = (float) (180d / Math.PI);
-    private static final float RADIUS = 250;
-    private Paint mGyroPaint = new Paint();
-    private Paint mGyroPaintY = new Paint();
-    private float mX, mY, mZ;
+    
+    private Paint mGyroPaint, mGyroPaintY;
+    private float mX, mY, mZ, mRadius;
 
-    public GyroVisualizer(Context context) {
-        this(context, null);
+    public GyroVisualizer(Context context){
+    	super(context, null, 0);
+        mGyroPaint = new Paint();
+        mGyroPaintY = new Paint();
+        
+        mGyroPaint.setColor(0x77000000);
+        mGyroPaint.setStyle(Style.STROKE);
+        mGyroPaint.setStrokeWidth(10);
+        mGyroPaint.setAntiAlias(true);
+        
+        mGyroPaintY.setColor(0x7700FF00);
+        mGyroPaintY.setStyle(Style.STROKE);
+        mGyroPaintY.setStrokeWidth(10);
+        mGyroPaintY.setAntiAlias(true);
     }
 
-    public GyroVisualizer(Context context, AttributeSet attrs) {
+    /*public GyroVisualizer(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
@@ -34,7 +45,7 @@ public class GyroVisualizer extends View {
         mGyroPaintY.setStyle(Style.STROKE);
         mGyroPaintY.setStrokeWidth(10);
         mGyroPaintY.setAntiAlias(true);
-    }
+    }*/
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -42,14 +53,15 @@ public class GyroVisualizer extends View {
 
         float midX = getWidth() / 2f;		//Finds center of phonescreen
         float midY = getHeight() / 2f;
+        mRadius = midY - 5f;	// Heigth of the circle
 
         // Gyroscope
         canvas.save();
         canvas.rotate(mZ * RADIANS_TO_DEGREES, midX, midY);
-        canvas.drawLine(midX, midY, midX, midY - RADIUS, mGyroPaintY);	
-        canvas.drawLine(midX, midY, midX, midY + RADIUS, mGyroPaint);
-        canvas.drawLine(midX - RADIUS, midY, midX + RADIUS, midY, mGyroPaint);
-        canvas.drawCircle(midX, midY, RADIUS, mGyroPaint);
+        canvas.drawLine(midX, midY, midX, midY - mRadius, mGyroPaintY);	
+        canvas.drawLine(midX, midY, midX, midY + mRadius, mGyroPaint);
+        canvas.drawLine(midX - mRadius, midY, midX + mRadius, midY, mGyroPaint);
+        canvas.drawCircle(midX, midY, mRadius, mGyroPaint);
         canvas.restore();
 
         canvas.drawCircle(midX + mY * 350, midY + mX * 350, 10, mGyroPaint);
